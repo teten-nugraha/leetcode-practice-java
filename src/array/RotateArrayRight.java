d8020fef68f29f138f95bf7c4c913e6c445fd04d
Teten Nugraha <teten.nugraha18@gmail.com>
2022-06-21 10:57:50 +0700

    rotate array right

--- src/array/RotateArrayRight.java
+++ src/array/RotateArrayRight.java
@@ -1,8 +1,30 @@
 package array;
 
+import java.util.Arrays;
+
 public class RotateArrayRight {
 	
 	public static void main(String[]args) {
+		int[]datas = new int[]{-1,-100,3,99};
+		int countRotate = 2;
+		rotate(datas, countRotate);
+		System.out.println(Arrays.toString(datas));
+	}
+
+	private static void rotate(int[] datas, int countRotate) {
+		
+		int arrLength = datas.length;
+		int lastElement;
+		
+		for(int i=0; i < countRotate; i++) {
+			lastElement = datas[arrLength-1];
+			
+			for(int j = arrLength-1; j > 0; j--) {
+				datas[j] = datas[j-1];
+			}
+			datas[0] = lastElement;
+			
+		}
 		
 	}
 	
